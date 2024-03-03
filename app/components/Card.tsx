185958625b2b960d6c09f9870211e1d7259a56bd
gabesone <sena6.gabriel@gmail.com>
2024-03-03 14:04:56 -0300

    add myfilms project

---
+++ app/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+const Card = () => {
+  return (
+    <div className="w-64 h-96 m-6 relative rounded">
+      <div>
+        <p>test</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
