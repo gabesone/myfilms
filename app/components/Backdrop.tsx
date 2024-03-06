b3aefecf11fcdf5b5700035923bec3cd4791d658
gabesone <sena6.gabriel@gmail.com>
2024-03-06 19:58:12 -0300

    add backdrop styles

--- app/components/Backdrop.tsx
+++ app/components/Backdrop.tsx
@@ -1,12 +1,12 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { HeroProps } from "../types";
 import Image from "next/image";
+import InfoIcon from "@mui/icons-material/Info";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const Backdrop = ({ backdrop, title, description, rating }: HeroProps) => {
   return (
-    <div className="relative w-[1080] h-[656px] shadow-2xl">
+    <div className="relative w-full shadow-2xl">
       <div className="absolute h-full w-[60%] top-0 left-0 bg-gradient-to-r from-black via-current to-transparent z-10 ">
         <div className="flex flex-col text-[#D5D9DC] py-32 px-8 space-y-4">
           <h2 className="text-4xl">{title}</h2>
@@ -19,9 +19,18 @@ const Backdrop = ({ backdrop, title, description, rating }: HeroProps) => {
               : rating.toFixed(1)}
             /10
           </p>
-          <div>
-            <button className="border p-2 transition hover:bg-[#416792] rounded">
+          <div className="space-x-4">
+            <button className="py-2 transition hover:bg-[#393a3b] bg-[#1A1C20] rounded px-4">
+              <div className="inline pr-2">
+                <PlayArrowIcon />
+              </div>
               Watch Trailer
+            </button>
+            <button className="py-2 transition hover:bg-[#333536] bg-[#464747] rounded px-4">
+              <div className="inline pr-2">
+                <InfoIcon />
+              </div>
+              More Info
             </button>
           </div>
         </div>
