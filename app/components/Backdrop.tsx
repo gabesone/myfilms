2747be49f09272a01712ba6a1456b1ec985ebc34
gabesone <sena6.gabriel@gmail.com>
2024-03-09 21:44:08 -0300

    add pathname to display dynamic information

--- app/components/Backdrop.tsx
+++ app/components/Backdrop.tsx
@@ -5,11 +5,14 @@ import { HeroProps } from "../types";
 import Image from "next/image";
 import InfoIcon from "@mui/icons-material/Info";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import { usePathname } from "next/navigation";
 
 import Link from "next/link";
 import Rating from "./Rating";
 
 const Backdrop = ({ backdrop, title, description, rating, id }: HeroProps) => {
+  const pathname = usePathname();
+
   return (
     <div className="relative w-full h-[720px] shadow-2xl">
       <div className="absolute h-full w-[60%] top-0 left-0 bg-gradient-to-r from-black via-current to-transparent z-10 ">
@@ -25,14 +28,18 @@ const Backdrop = ({ backdrop, title, description, rating, id }: HeroProps) => {
               </div>
               Watch Trailer
             </button>
-            <Link href={`/films/${id}`}>
-              <button className="py-2 transition hover:bg-[#333536] bg-[#464747] rounded px-4">
-                <div className="inline pr-2">
-                  <InfoIcon />
-                </div>
-                More Info
-              </button>
-            </Link>
+            {pathname === `/films/${id}` ? (
+              ""
+            ) : (
+              <Link href={`/films/${id}`}>
+                <button className="py-2 transition hover:bg-[#333536] bg-[#464747] rounded px-4">
+                  <div className="inline pr-2">
+                    <InfoIcon />
+                  </div>
+                  More Info
+                </button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
